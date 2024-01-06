70ddaf84337a6a15c76e0b29e3d7285bd406a5eb
Alexandra Bunch <121991056+oldfashionedjelly@users.noreply.github.com>
2024-01-05 19:46:59 -0500

    Create scientificNotation.java

---
+++ scientificNotation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+
+class Main {
+  public static void main(String[] args) {
+		String scientific = "1.56 * 10^4";
+		int standard = 136545000;
+		System.out.println(fromScientific(scientific));
+  }
+
+  public static String toScientific(int num) {
+		return "";
+  }
+
+  public static double fromScientific(String num) {
+		return 0.0;
+  }
+}
